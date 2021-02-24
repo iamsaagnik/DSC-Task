db1ff7eb5cdc490b1686a9bc83ec70d95ef43f0f
saagnik karmakar <71928668+iamsaagnik@users.noreply.github.com>
2021-02-24 20:48:51 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does DSC stand for?",
+    answer: "Developer Student Club",
+    options: [
+      "Developer Student Club",
+      "Developer SRM Club",
+      "Developer SRM Commitee",
+      "Developer Student Community"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Full form of CSS?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+];
